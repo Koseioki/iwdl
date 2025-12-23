1d51232b52e004e8fe9700d1db9e17963a16d327
Kosei Oki <koseioki1225@gmail.com>
2025-12-23 16:41:13 +0100

    header layout

--- iwdl/src/Components/Global/Footer.tsx
+++ iwdl/src/Components/Global/Footer.tsx
@@ -2,7 +2,9 @@ import './Footer.css';
 export function Footer() {
     return (
         <footer>
+            <div className="footer-content">
             <p>© 2024 Intersectional Web Design Lab</p>
+            </div>
         </footer>
     );
 }
--- iwdl/src/Components/Global/Header.tsx
+++ iwdl/src/Components/Global/Header.tsx
@@ -4,16 +4,26 @@ import "./Header.css";
 export function Header() {
   return (
     <header>
-      <NavLink className="logo" to="/">Intersectional Web Design Lab</NavLink>
+      <NavLink className="logo" to="/">
+        Intersectional Web Design Lab
+      </NavLink>
 
       <nav className="header-nav">
         <ul>
-        <li><NavLink to="/what-is-intersectional-web-design">
-          What is Intersectional Web Design?
-        </NavLink></li>
-        <li><NavLink to="/design-in-practice">Design in Practice</NavLink></li>
-        <li><NavLink to="/resources-and-events">Resources & Events</NavLink></li>
-        <li><NavLink to="/about-us">About Us</NavLink></li>
+          <li>
+            <NavLink to="/what-is-intersectional-web-design">
+              What is Intersectional Web Design?
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/design-in-practice">Design in Practice</NavLink>
+          </li>
+          <li>
+            <NavLink to="/resources-and-events">Resources & Events</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about-us">About Us</NavLink>
+          </li>
         </ul>
       </nav>
     </header>
