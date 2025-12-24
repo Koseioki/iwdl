6091c9d3cef925c6a1380cc17762d1c78d806725
Kosei Oki <koseioki1225@gmail.com>
2025-12-24 17:18:41 +0100

    transition to IDL

---
+++ src/Components/Global/Footer.tsx
@@ -0,0 +1,10 @@
+import './Footer.css';
+export function Footer() {
+    return (
+        <footer>
+            <div className="footer-content">
+            <p>© 2025 Intersectional Design Lab</p>
+            </div>
+        </footer>
+    );
+}
